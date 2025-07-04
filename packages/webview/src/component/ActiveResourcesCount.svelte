3c53c70dc56b7f9977c09e84487a6b4444a86e97
Philippe Martin <phmartin@redhat.com>
2025-07-04 11:11:33 +0200

    fix: add some css to pages (#37)
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/webview/src/component/ActiveResourcesCount.svelte
+++ packages/webview/src/component/ActiveResourcesCount.svelte
@@ -12,12 +12,14 @@ onMount(() => {
 
 <NavPage title="Active resources count" searchEnabled={false}>
   {#snippet content()}
-    {#if activeResourcesCount.data?.counts}
-      <ul>
-        {#each activeResourcesCount.data.counts as count, index (index)}
-          <li>{count.contextName}/{count.resourceName}: {count.count}</li>
-        {/each}
-      </ul>
-    {/if}
+    <main class="flex flex-col w-screen h-screen overflow-auto bg-[var(--pd-content-bg)] text-base m-4">
+      {#if activeResourcesCount.data?.counts}
+        <ul class="list-disc list-inside">
+          {#each activeResourcesCount.data.counts as count, index (index)}
+            <li>{count.contextName}/{count.resourceName}: {count.count}</li>
+          {/each}
+        </ul>
+      {/if}
+    </main>
   {/snippet}
 </NavPage>
--- packages/webview/src/component/ResourcesCount.svelte
+++ packages/webview/src/component/ResourcesCount.svelte
@@ -12,12 +12,14 @@ onMount(() => {
 
 <NavPage title="Resources count" searchEnabled={false}>
   {#snippet content()}
-    {#if resourcesCount.data?.counts}
-      <ul>
-        {#each resourcesCount.data.counts as count, index (index)}
-          <li>{count.contextName}/{count.resourceName}: {count.count}</li>
-        {/each}
-      </ul>
-    {/if}
+    <main class="flex flex-col w-screen h-screen overflow-auto bg-[var(--pd-content-bg)] text-base m-4">
+      {#if resourcesCount.data?.counts}
+        <ul class="list-disc list-inside">
+          {#each resourcesCount.data.counts as count, index (index)}
+            <li>{count.contextName}/{count.resourceName}: {count.count}</li>
+          {/each}
+        </ul>
+      {/if}
+    </main>
   {/snippet}
 </NavPage>
