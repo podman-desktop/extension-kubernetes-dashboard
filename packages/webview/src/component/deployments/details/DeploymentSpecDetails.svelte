5dc62bdd097026375e673f2a6d9ce98f7437e5db
Philippe Martin <phmartin@redhat.com>
2025-08-20 15:34:33 +0200

    feat: deployment details
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/webview/src/component/deployments/details/DeploymentSpecDetails.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+import type { V1DeploymentSpec } from '@kubernetes/client-node';
+
+import Title from '/@/component/details/Title.svelte';
+import Cell from '/@/component/details/Cell.svelte';
+import Subtitle from '/@/component/details/Subtitle.svelte';
+import ContainerDetails from './ContainerDetails.svelte';
+import { WorkloadKind } from './workload-kind';
+
+interface Props {
+  spec: V1DeploymentSpec;
+  name?: string;
+  namespace?: string;
+}
+let { spec, name, namespace }: Props = $props();
+</script>
+
+<tr>
+  <Title>Details</Title>
+</tr>
+<tr>
+  <Cell>Replicas</Cell>
+  <Cell>{spec.replicas}</Cell>
+</tr>
+{#if spec.selector.matchLabels}
+  <tr>
+    <Cell>Selector</Cell>
+    <Cell>
+      {#each Object.entries(spec.selector.matchLabels) as [key, value] (key)}
+        <div>{key}: {value}</div>
+      {/each}
+    </Cell>
+  </tr>
+{/if}
+{#if spec.strategy}
+  <tr>
+    <Cell>Strategy</Cell>
+    <Cell>{spec.strategy.type}</Cell>
+  </tr>
+{/if}
+{#if spec.template.spec?.containers}
+  <tr>
+    <Title>Containers</Title>
+  </tr>
+  {#each spec.template.spec?.containers as container (container.name)}
+    <tr>
+      <Subtitle>{container.name}</Subtitle>
+    </tr>
+    <ContainerDetails kind={WorkloadKind.DEPLOYMENT} namespace={namespace} resourceName={name} artifact={container} />
+  {/each}
+{/if}
