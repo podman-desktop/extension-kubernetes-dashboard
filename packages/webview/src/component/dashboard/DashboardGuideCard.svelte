929d386c292b630aabd6c46e83425bd879ab20fa
Philippe Martin <phmartin@redhat.com>
2025-08-27 16:08:52 +0200

    feat: add links and docs to Dashboard (#205)
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/webview/src/component/dashboard/DashboardGuideCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { Button } from '@podman-desktop/ui-svelte';
+import { getContext } from 'svelte';
+import { Remote } from '/@/remote/remote';
+import { API_SYSTEM } from '/@common/channels';
+
+interface Props {
+  title: string;
+  link: string;
+  image: string;
+}
+
+let { title, link, image }: Props = $props();
+
+const remote = getContext<Remote>(Remote);
+const systemApi = remote.getProxy(API_SYSTEM);
+
+async function openLink(): Promise<void> {
+  await systemApi.openExternal(link);
+}
+</script>
+
+<div
+  class="flex flex-col gap-4 p-4 bg-[var(--pd-content-card-carousel-card-bg)] hover:bg-[var(--pd-content-card-carousel-card-hover-bg)] rounded-md items-center">
+  <img src={image} class="max-w-[100%] object-contain rounded-md self-start" alt={`${title} image`} />
+  <div class="text-[var(--pd-content-card-carousel-card-header-text)] text-center font-semibold">{title}</div>
+  <Button on:click={openLink}>Read more</Button>
+</div>
