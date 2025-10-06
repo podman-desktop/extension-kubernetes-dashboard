9e6c54e34eaf44b814c019f357ce1cf1164eede8
Marcel Bertagnini <mbertagn@redhat.com>
2025-10-06 12:20:43 +0200

    feat: added cronstrue to have readable schedules
    Signed-off-by: Marcel Bertagnini <mbertagn@redhat.com>

---
+++ packages/webview/src/component/cronjobs/columns/Schedule.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import cronstrue from 'cronstrue';
+
+interface Props {
+  object: string;
+}
+
+let { object }: Props = $props();
+
+function getScheduleReadableText(schedule: string): string {
+  try {
+    return cronstrue.toString(schedule);
+  } catch (error) {
+    return schedule;
+  }
+}
+</script>
+
+<div class="text-[var(--pd-table-body-text)] max-w-full text-wrap line-clamp-2">
+  <span>{getScheduleReadableText(object)}</span>
+</div>
