f72c71fcb70a3b360d0f12e5ed980af2d1a00f19
Philippe Martin <phmartin@redhat.com>
2025-10-15 14:36:51 +0200

    test: add unit tests
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/webview/src/component/dashboard/KubernetesProviderCard.spec.ts
@@ -0,0 +1,69 @@
+/**********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom/vitest';
+
+import { render, screen } from '@testing-library/svelte';
+import { beforeEach, expect, test, vi } from 'vitest';
+import KubernetesProviderCard from '/@/component/dashboard/KubernetesProviderCard.svelte';
+import { RemoteMocks } from '/@/tests/remote-mocks';
+import { API_NAVIGATION } from '@kubernetes-dashboard/channels';
+import type { NavigationApi } from '@kubernetes-dashboard/channels';
+import userEvent from '@testing-library/user-event';
+
+const remoteMocks = new RemoteMocks();
+
+beforeEach(() => {
+  vi.resetAllMocks();
+  remoteMocks.reset();
+
+  remoteMocks.mock(API_NAVIGATION, {
+    navigateToProviderNewConnection: vi.fn(),
+  } as unknown as NavigationApi);
+});
+
+test('should render with all values passed', async () => {
+  render(KubernetesProviderCard, {
+    provider: {
+      id: 'k8s-provider',
+      creationDisplayName: 'Kubernetes Provider',
+      creationButtonTitle: 'Create Kubernetes Provider',
+      emptyConnectionMarkdownDescription: 'Create a new Kubernetes Provider',
+    },
+  });
+  screen.getByText('Kubernetes Provider');
+  screen.getByText('Create Kubernetes Provider');
+  const btn = screen.getByRole('button', { name: 'Create Kubernetes Provider' });
+  expect(btn).toBeEnabled();
+  await userEvent.click(btn);
+  expect(remoteMocks.get(API_NAVIGATION).navigateToProviderNewConnection).toHaveBeenCalledWith('k8s-provider');
+});
+
+test('should render with minimal values passed', async () => {
+  render(KubernetesProviderCard, {
+    provider: {
+      id: 'k8s-provider',
+    },
+  });
+  screen.getByText('Create');
+  screen.getByText('Create new');
+  const btn = screen.getByRole('button', { name: 'Create new' });
+  expect(btn).toBeEnabled();
+  await userEvent.click(btn);
+  expect(remoteMocks.get(API_NAVIGATION).navigateToProviderNewConnection).toHaveBeenCalledWith('k8s-provider');
+});
