b45413ac3907e1770810aa4fa4576cbe7d2989d5
Philippe Martin <phmartin@redhat.com>
2025-10-29 08:03:44 +0100

    List kubernetes providers in "no context" page (#368)
    * feat: display kubernetes renderers
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * feat: simple markdown component
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * test: add unit tests
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    ---------
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/webview/src/component/dashboard/NoContextPage.svelte
+++ packages/webview/src/component/dashboard/NoContextPage.svelte
@@ -1,16 +1,38 @@
 <script lang="ts">
 import KubeIcon from '/@/component/icons/KubeIcon.svelte';
+import { getContext, onDestroy, onMount } from 'svelte';
+import { States } from '/@/state/states';
+import type { Unsubscriber } from 'svelte/store';
+import KubernetesProviderCard from '/@/component/dashboard/KubernetesProviderCard.svelte';
+
+const states = getContext<States>(States);
+const kubernetesProviders = states.stateKubernetesProvidersInfoUI;
+
+let unsubscribers: Unsubscriber[] = [];
+onMount(() => {
+  unsubscribers.push(kubernetesProviders.subscribe());
+});
+
+onDestroy(() => {
+  unsubscribers.forEach(unsubscriber => unsubscriber());
+  unsubscribers = [];
+});
 </script>
 
 <div class="mt-8 flex justify-center overflow-auto">
-  <div class="max-w-[600px] flex flex-col text-center space-y-3">
+  <div class="max-w-[800px] flex flex-col text-center content-center space-y-3">
     <div class="flex justify-center text-[var(--pd-details-empty-icon)] py-2">
       <KubeIcon size="80" />
     </div>
     <h1 class="text-xl text-[var(--pd-details-empty-header)]">No Kubernetes cluster</h1>
-    <div class="text-[var(--pd-details-empty-sub-header)] text-pretty">
+    <div class="text-[var(--pd-details-empty-sub-header)] text-balance">
       A Kubernetes cluster is a group of nodes (virtual or physical) that run Kubernetes, a system for automating the
       deployment and management of containerized applications.
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-2 justify-center">
+      {#each kubernetesProviders.data?.providers as provider (provider.id)}
+        <KubernetesProviderCard provider={provider} />
+      {/each}
     </div>
   </div>
 </div>
