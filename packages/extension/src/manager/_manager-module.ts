5f8742a4d62c51301fc82ea4867a4d7c1aecfc05
Philippe Martin <phmartin@redhat.com>
2026-01-29 14:24:54 +0100

    feat: telemetry api (#665)
    * feat: telemetry api
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * fix: copyright year
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    ---------
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/extension/src/manager/_manager-module.ts
+++ packages/extension/src/manager/_manager-module.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2025 Red Hat, Inc.
+ * Copyright (C) 2025 - 2026 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import { IDisposable } from '@kubernetes-dashboard/channels';
 import { PodTerminalsApiImpl } from './pod-terminals-api-impl';
 import { NavigationApiImpl } from '/@/manager/navigation-api';
 import { KubernetesProvidersManager } from '/@/manager/kubernetes-providers';
+import { TelemetryApiImpl } from './telemetry-api';
 
 const managersModule = new ContainerModule(options => {
   options.bind<ContextsManager>(ContextsManager).toSelf().inSingletonScope();
@@ -37,6 +38,7 @@ const managersModule = new ContainerModule(options => {
   options.bind<PodTerminalsApiImpl>(PodTerminalsApiImpl).toSelf().inSingletonScope();
   options.bind<NavigationApiImpl>(NavigationApiImpl).toSelf().inSingletonScope();
   options.bind<KubernetesProvidersManager>(KubernetesProvidersManager).toSelf().inSingletonScope();
+  options.bind<TelemetryApiImpl>(TelemetryApiImpl).toSelf().inSingletonScope();
 
   // Bind IDisposable to services which need to clear data/stop connection/etc when the panel is left
   // (the onDestroy are not called from components when the panel is left, which may introduce memory leaks if not disposed here)
