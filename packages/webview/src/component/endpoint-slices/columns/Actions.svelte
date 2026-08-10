8e03d1f4a19598d42e42904ce86f129a62dc4a1a
Charlie Drage <charlie@charliedrage.com>
2026-08-10 10:30:46 -0400

    chore(refactor): update / add missing parts from skills
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/webview/src/component/endpoint-slices/columns/Actions.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-import DeleteAction from '/@/component/objects/columns/DeleteAction.svelte';
-import type { Props } from './props';
-
-let { object }: Props = $props();
-</script>
-
-<DeleteAction object={object} />
