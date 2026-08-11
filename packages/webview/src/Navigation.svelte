3bf72a550fd016bd6cb7a2e6df94bec4a34dc6c1
Charlie Drage <charlie@charliedrage.com>
2026-08-11 11:59:01 -0400

    fix: format files with prettier
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/webview/src/Navigation.svelte
+++ packages/webview/src/Navigation.svelte
@@ -47,6 +47,7 @@ const configUrls = [
   navigator.kubernetesResourcesURL('ConfigMap'),
   navigator.kubernetesResourcesURL('RuntimeClass'),
 ];
+const configUrls = [navigator.kubernetesResourcesURL('ConfigMap'), navigator.kubernetesResourcesURL('RuntimeClass')];
 
 const networkUrls = [
   navigator.kubernetesResourcesURL('Service'),
