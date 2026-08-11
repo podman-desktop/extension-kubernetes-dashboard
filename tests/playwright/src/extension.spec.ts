5374e22b2c5cd91f4914be18a055a6dafff7a1dc
Charlie Drage <charlie@charliedrage.com>
2026-08-11 10:54:23 -0400

    fix: format files with prettier
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- tests/playwright/src/extension.spec.ts
+++ tests/playwright/src/extension.spec.ts
@@ -220,7 +220,9 @@ test.describe(`Extension usage`, { tag: '@integration' }, () => {
   test('go to validatingWebhooks page', async () => {
     const validatingWebhooksPage = await navigation.openTabPage(KubernetesResources.ValidatingWebhookConfigs);
     await playExpect(validatingWebhooksPage.heading).toBeVisible();
-    await playExpect.poll(async () => validatingWebhooksPage.isEmpty('No validatingwebhookconfigurations')).toBeTruthy();
+    await playExpect
+      .poll(async () => validatingWebhooksPage.isEmpty('No validatingwebhookconfigurations'))
+      .toBeTruthy();
   });
 
   test('go to configmaps & secrets page', async () => {
