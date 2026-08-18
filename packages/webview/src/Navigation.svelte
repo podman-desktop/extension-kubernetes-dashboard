0fe7d52e06a6a12c260894f39028521b7ae7168d
Charlie Drage <charlie@charliedrage.com>
2026-08-18 15:45:50 -0400

    fix: resolve broken rebase in shared files
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/webview/src/Navigation.svelte
+++ packages/webview/src/Navigation.svelte
@@ -42,12 +42,7 @@ const workloadUrls = [
   navigator.kubernetesResourcesURL('CronJob'),
 ];
 
-const configUrls = [navigator.kubernetesResourcesURL('ConfigMap'), navigator.kubernetesResourcesURL('ServiceAccount')];
-const configUrls = [
-  navigator.kubernetesResourcesURL('ConfigMap'),
-  navigator.kubernetesResourcesURL('Lease'),
-];
-const configUrls = [navigator.kubernetesResourcesURL('ConfigMap'), navigator.kubernetesResourcesURL('Lease')];
+const configUrls = [navigator.kubernetesResourcesURL('ConfigMap'), navigator.kubernetesResourcesURL('ServiceAccount'), navigator.kubernetesResourcesURL('Lease')];
 
 const networkUrls = [
   navigator.kubernetesResourcesURL('Service'),
