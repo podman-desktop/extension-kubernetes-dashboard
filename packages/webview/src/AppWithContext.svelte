e76321c0c4946eb152ee9b2d76d1f4b032e140f7
Charlie Drage <charlie@charliedrage.com>
2026-08-18 15:45:48 -0400

    fix: resolve broken rebase in shared files
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/webview/src/AppWithContext.svelte
+++ packages/webview/src/AppWithContext.svelte
@@ -235,6 +235,8 @@ const { meta }: Props = $props();
 
   <Route path="/serviceaccounts/:name/:namespace/*" let:meta>
     <ServiceAccountDetails name={decodeURI(meta.params.name)} namespace={decodeURI(meta.params.namespace)} />
+  </Route>
+
   <Route path="/runtimeclasses">
     <RuntimeClassesList />
   </Route>
--- packages/webview/src/Navigation.svelte
+++ packages/webview/src/Navigation.svelte
@@ -52,7 +52,6 @@ const configUrls = [
   navigator.kubernetesResourcesURL('PriorityClass'),
   navigator.kubernetesResourcesURL('RuntimeClass'),
 ];
-const configUrls = [navigator.kubernetesResourcesURL('ConfigMap'), navigator.kubernetesResourcesURL('RuntimeClass')];
 
 const networkUrls = [
   navigator.kubernetesResourcesURL('Service'),
