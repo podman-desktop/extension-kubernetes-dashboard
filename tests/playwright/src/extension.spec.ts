399b2a4f97a424cb6547711d4b540dc73206ff6a
Charlie Drage <charlie@charliedrage.com>
2026-08-18 16:06:13 -0400

    chore: fix formatting and lint issues
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- tests/playwright/src/extension.spec.ts
+++ tests/playwright/src/extension.spec.ts
@@ -256,6 +256,7 @@ test.describe(`Extension usage`, { tag: '@integration' }, () => {
     const serviceAccountsPage = await navigation.openTabPage(KubernetesResources.ServiceAccounts);
     await playExpect(serviceAccountsPage.heading).toBeVisible();
     await playExpect.poll(async () => serviceAccountsPage.isEmpty('No serviceaccounts')).toBeTruthy();
+  });
   test('go to limitRanges page', async () => {
     const limitRangesPage = await navigation.openTabPage(KubernetesResources.LimitRanges);
     await playExpect(limitRangesPage.heading).toBeVisible();
