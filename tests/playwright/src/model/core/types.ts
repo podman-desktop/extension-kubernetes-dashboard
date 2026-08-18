ebe2b641d170b39768b682074849effe3daca5fc
Charlie Drage <charlie@charliedrage.com>
2026-08-18 16:42:51 -0400

    chore: fix formatting and lint issues
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- tests/playwright/src/model/core/types.ts
+++ tests/playwright/src/model/core/types.ts
@@ -92,7 +92,15 @@ export const KubernetesResourceAttributes: Record<KubernetesResources, string[]>
   ],
   [KubernetesResources.Jobs]: ['Selected', 'Status', 'Name', 'Conditions', 'Completions', 'Age', 'Actions'],
   [KubernetesResources.ServiceAccounts]: ['Selected', 'Status', 'Name', 'Secrets', 'Age', 'Actions'],
-  [KubernetesResources.ValidatingWebhookConfigs]: ['Selected', 'Status', 'Name', 'Webhooks', 'Failure Policy', 'Age', 'Actions'],
+  [KubernetesResources.ValidatingWebhookConfigs]: [
+    'Selected',
+    'Status',
+    'Name',
+    'Webhooks',
+    'Failure Policy',
+    'Age',
+    'Actions',
+  ],
   [KubernetesResources.ValidatingWebhookConfigs]: [
     'Selected',
     'Status',
