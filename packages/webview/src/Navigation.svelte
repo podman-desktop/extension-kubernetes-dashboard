1d72b0b6da01e311a361f6444e1867af66840ea7
Charlie Drage <charlie@charliedrage.com>
2026-08-18 16:07:39 -0400

    chore: fix formatting and lint issues
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/webview/src/Navigation.svelte
+++ packages/webview/src/Navigation.svelte
@@ -42,7 +42,11 @@ const workloadUrls = [
   navigator.kubernetesResourcesURL('CronJob'),
 ];
 
-const configUrls = [navigator.kubernetesResourcesURL('ConfigMap'), navigator.kubernetesResourcesURL('ServiceAccount'), navigator.kubernetesResourcesURL('HorizontalPodAutoscaler')];
+const configUrls = [
+  navigator.kubernetesResourcesURL('ConfigMap'),
+  navigator.kubernetesResourcesURL('ServiceAccount'),
+  navigator.kubernetesResourcesURL('HorizontalPodAutoscaler'),
+];
 
 const networkUrls = [
   navigator.kubernetesResourcesURL('Service'),
