8b2214e83785c4099d0744f512ef7432271aedbd
Philippe Martin <phmartin@redhat.com>
2026-09-04 23:00:26 +0200

    fix: duplicate entry in navigation
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/webview/src/Navigation.svelte
+++ packages/webview/src/Navigation.svelte
@@ -176,7 +176,6 @@ $effect(() => {
       <NavItem title="Priority Classes" child={true} href={navigator.kubernetesResourcesURL('PriorityClass')} />
       <NavItem title="Runtime Classes" child={true} href={navigator.kubernetesResourcesURL('RuntimeClass')} />
       <NavItem title="Leases" child={true} href={navigator.kubernetesResourcesURL('Lease')} />
-      <NavItem title="Mutating Webhook Configs" child={true} href={navigator.kubernetesResourcesURL('MutatingWebhookConfiguration')} />
       <NavItem
         title="Mutating Webhook Configs"
         child={true}
